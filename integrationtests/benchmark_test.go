d3b40512962f305881e3a630a1be4fd790e60f74
ATMackay <alexandertmackay@hotmail.co.uk>
2024-02-19 12:26:33 +0000

    test: add transaction write benchmark test

--- integrationtests/benchmark_test.go
+++ integrationtests/benchmark_test.go
@@ -99,3 +99,53 @@ func BenchmarkAccountRead(b *testing.B) {
 	}
 
 }
+
+func BenchmarkTransactionWrite(b *testing.B) {
+
+	// Setup
+
+	serverURL := "http://0.0.0.0:8080"
+
+	// Healthcheck the stack
+	response, err := executeRequest(http.MethodGet, serverURL+service.Health, nil, http.StatusOK)
+	if err != nil {
+		b.Fatal(err)
+	}
+	response.Body.Close()
+
+	// Write a User 1 Account to the DB
+	accParams := database.CreateAccountParams{Username: "myusername", Email: sql.NullString{String: "myemail@provider.com"}}
+	accBytes, err := json.Marshal(accParams)
+	if err != nil {
+		b.Fatal(err)
+	}
+	response, _ = executeRequest(http.MethodPut, serverURL+service.CreateAccount, bytes.NewReader(accBytes), http.StatusOK)
+	if response != nil {
+		response.Body.Close()
+	}
+
+	// Write a User 2 Account to the DB
+	accParams = database.CreateAccountParams{Username: "yourusername", Email: sql.NullString{String: "youremail@provider.com"}}
+	accBytes, err = json.Marshal(accParams)
+	if err != nil {
+		b.Fatal(err)
+	}
+	response, _ = executeRequest(http.MethodPut, serverURL+service.CreateAccount, bytes.NewReader(accBytes), http.StatusOK)
+	if response != nil {
+		response.Body.Close()
+	}
+
+	for n := 0; n < b.N; n++ {
+		// Prepare tx
+		TxData := database.Transaction{FromAccount: sql.NullInt64{Int64: 1}, ToAccount: sql.NullInt64{Int64: 2}, Amount: sql.NullInt64{Int64: 1}, CreatedAt: sql.NullTime{Time: time.Now()}}
+		TxB, err := json.Marshal(TxData)
+		if err != nil {
+			b.Fatal(err)
+		}
+		// Write transaction
+		if _, err = executeRequest(http.MethodPut, serverURL+service.CreateTx, bytes.NewReader(TxB), http.StatusOK); err != nil {
+			b.Fatal(err)
+		}
+	}
+
+}
