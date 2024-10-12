8c08bdcff11ddda558fd78a14ccaa921d39fc5fd
ATMackay <amackay@alluvial.finance>
2024-10-12 13:14:34 +0100

    fix lint

--- service/http.go
+++ service/http.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"net/http"
@@ -195,7 +196,7 @@ func HandleResponseErr(resp *http.Response) error {
 		if err := DecodeJSON(resp.Body, &v); err != nil {
 			return fmt.Errorf("cannot parse JSON body from error response: %w", err)
 		}
-		return fmt.Errorf(v.Err)
+		return errors.New(v.Err)
 	}
 	return nil
 }
